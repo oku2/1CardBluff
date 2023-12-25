861073358abfda3ff74744286af5a1dabbfccfd3
Oiva Kupila <oiva.a.kupila@utu.fi>
2023-12-25 17:16:55 +0200

    added logic to choose player count

---
+++ logic.js
@@ -0,0 +1,11 @@
+let playerCount = null;
+
+
+function players(number) {
+    playerCount = number;
+    var pselect = document.getElementById("pselect");
+    pselect.style.display = "none";
+    var rwindow = document.getElementById("rwindow");
+    rwindow.style.display = "grid";
+    
+  }
